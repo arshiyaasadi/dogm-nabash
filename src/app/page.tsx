29e9863ab63efdf29d9d7db3bef181ae9e4561a7
Arshiya Asadi <arasadi@sepehrpay.com>
2025-01-06 17:24:40 +0330

    init base info and page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,66 @@
-import Image from "next/image";
+import Image from 'next/image'
+import { IoLogoInstagram, IoLogoYoutube } from 'react-icons/io'
+import { AiOutlineVideoCamera } from 'react-icons/ai'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="relative w-full h-screen bg-banner-ps bg-repeat">
+      <header className="relative top-0 left-0 right-0 h-40 md:h-48 lg:h-56 xl:h-60 2xl:h-64 max-w-7xl mx-auto border-0 xl:border-x-8 xl:border-solid xl:border-outline xl:border-y-0">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          // shadow-blurred-banner
+          className=""
+          src="/images/dogm_nabash_banner.jpg"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+          alt="Dogm Nabash - دگم نباش"
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+      <div className="fixed bottom-0 left-0">
+        <AiOutlineVideoCamera color="black" size="48" />
+      </div>
+
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start max-w-7xl mx-auto bg-table-ps bg-repeat p-4 border-0 border-t-2 xl:border-8 xl:border-solid xl:border-outline">
+        <div className="w-full flex items-center justify-between">
+          <div className="relative rounded-full overflow-hidden w-24 h-24 md:h-32 md:w-32 shadow-2xl">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/images/dogm_nabash_logo.jpg"
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center"
+              alt="dogm_nabash_logo"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <h1 className="text-surface-on font-extrabold text-2xl">Dogm Nabash - دگم نباش</h1>
+            {/* <h2></h2> */}
+            <span className="text-black">Coming soon ...</span>
+
+            <div className="w-auto flex gap-2">
+              <a
+                className="flex items-center gap-2 hover:underline hover:underline-offset-4 p-2"
+                href="https://instagram.com/thekouman"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <IoLogoInstagram color="black" size={24} />
+              </a>
+
+              <a
+                className="flex items-center gap-2 hover:underline hover:underline-offset-4 p-2"
+                href="https://youtube.com/@dogmnabash?si=HvklkymemjkBS0vA"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <IoLogoYoutube color="black" size={24} />
+              </a>
+            </div>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
-  );
+  )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,63 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        transparent: 'transparent',
+        primary: {
+          DEFAULT: 'var(--color-primary)',
+          on: 'var(--color-on-primary)',
+        },
+        secondary: {
+          DEFAULT: 'var(--color-secondary)',
+          on: 'var(--color-on-secondary)',
+        },
+        surface: {
+          DEFAULT: 'var(--color-surface)',
+          bright: 'var(--color-surface-bright)',
+          dim: 'var(--color-surface-dim)',
+          on: {
+            DEFAULT: 'var(--color-on-surface)',
+            variant: 'var(--color-on-surface-variant)',
+          },
+        },
+        outline: {
+          DEFAULT: 'var(--color-outline)',
+          variant: 'var(--color-outline-variant)',
+        },
+        success: {
+          DEFAULT: 'var(--color-success)',
+          on: 'var(--color-on-success)',
+        },
+        error: {
+          DEFAULT: 'var(--color-error)',
+          on: 'var(--color-on-error)',
+        },
+        warning: {
+          DEFAULT: 'var(--color-warning)',
+          on: 'var(--color-on-warning)',
+        },
+        info: {
+          DEFAULT: 'var(--color-info)',
+          on: 'var(--color-on-info)',
+        },
+        light: 'var(--color-light)',
+        dark: 'var(--color-dark)',
+      },
+      backgroundImage: {
+        'banner-ps': "url('/images/bg_ps.jpg')",
+        'table-ps': "url('/images/bg_table_ps.jpg')",
+      },
+      boxShadow: {
+        'blurred-banner': '0 0 20px 20px rgb(91 93 27 / 81%)',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
